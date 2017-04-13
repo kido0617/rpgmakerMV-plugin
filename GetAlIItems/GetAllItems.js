fb8d8652c7d7996bbb8d5fcc4ac93367c66c44bc
kido0617 <enokido0617@gmail.com>
2017-04-13 21:03:32 +0900

    get all items

---
+++ GetAlIItems/GetAllItems.js
@@ -0,0 +1,63 @@
+/*---------------------------------------------------------------------------*
+ * 2017/04/13 kido0617
+ * https://kido.space/
+ *---------------------------------------------------------------------------*/
+
+/*:
+ * @plugindesc 全アイテム取得プラグイン
+ * @author kido0617
+ *
+ * @help
+ *   Plug-in to get all weapons, armors, items
+ *   plugin command:
+ *   GetAllItems item 99      # Earn 99 items for all items
+ *   GetAllItems armor 1      # Earn 99 armors for all items
+ *   GetAllItems weapon 1     # Earn 99 weapons for all items
+ */
+
+/*:ja
+ * @plugindesc 全アイテム取得プラグイン
+ * @author kido0617
+ *
+ * @help
+ *   全武器、防具、アイテムを取得するプラグイン
+ *   プラグインコマンド:
+ *   GetAllItems item 99      # 全アイテムを99個獲得
+ *   GetAllItems armor 1      # 全防具を1個獲得
+ *   GetAllItems weapon 1     # 全武器を1個獲得
+ */
+
+(function(){
+  var parameters = PluginManager.parameters('GetAllItems');
+  var needDefaultItems = String(parameters['NeedDefaultItems'] || 'yes');
+  var needDefaultArmors = String(parameters['NeedDefaultArmors'] || 'yes');
+  var needDefaultWeapons = String(parameters['NeedDefaultWeapons'] || 'yes');
+  var defaultNum = String(parameters['DefaultNum'] || 1);
+
+  var _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+  Game_Interpreter.prototype.pluginCommand = function(command, args) {
+    _Game_Interpreter_pluginCommand.call(this, command, args);
+    if (command === 'GetAllItems') {
+      switch (args[0]) {
+      case 'item':
+        getAlls($dataItems, Number(args[1]));
+        break;
+      case 'armor':
+        getAlls($dataArmors, Number(args[1]));
+        break;
+      case 'weapon':
+        getAlls($dataWeapons, Number(args[1]));
+        break;
+      }
+    }
+  };
+
+  function getAlls(data, num){
+    for(var i = 1 ; i < data.length; i++){
+      if(data[i].name == "")continue;
+      $gameParty.gainItem(data[i], num);
+    }
+  }
+
+})();
+
